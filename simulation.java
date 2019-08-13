b6578665db11f8d443093c3623ec0227962d9f2d
utkarshyadavin <utkarshyadavin@gmail.com>
2019-08-14 01:11:13 +0530

    Completed a simple simulator that associates UEs to BSTs based on nearest distance

--- simulation.java
+++ simulation.java
@@ -1,7 +1,8 @@
 import java.util.*;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
 
 class BaseStation{
-
 	int xCoordinate; 
 	int yCoordinate;
 	int id; 
@@ -56,20 +57,20 @@ class BaseStation{
 
 }
 
-
-
 class UserEquipment{
 
 	int xCoordinate; 
 	int yCoordinate;
 	int id; 
 	int assignedBSId;
+	double assignedBSDist;
 
 	public UserEquipment(){
 		this.xCoordinate = 0; 
 		this.yCoordinate = 0; 
 		this.id = -1; 
 		this.assignedBSId = -1;
+		this.assignedBSDist = 0;
 	}
 
 	// Get UserEquipment Id
@@ -112,7 +113,17 @@ class UserEquipment{
 		this.assignedBSId = value;
 	}
 
+	// Getting distance of assigned BaseStation
+	public double getassignedBSDist(){
+		return assignedBSDist;
+	}
+
+	// Setting distance of assigned BS to UE
+	public void setassignedBSDist(double value){
+		this.assignedBSDist = value;
+	}
 }
+
 
 
 class Simulation{
@@ -123,14 +134,11 @@ class Simulation{
 		return temp;
 	}
 
-
 	public static void main(String args[]){
 
-		System.out.println("Ruuning Cellular Wireless Network Simulation");
 		// Maps to keep track of coordinates of BSTs and UEs to avoid repetition
 		HashMap<Integer, Integer> baseStationPosition = new HashMap<>();
 		HashMap<Integer,Integer> userEquipmentPosition = new HashMap<>();
-
 
 		// Create 50 Base Stations with ids ranging from 1 to 50
 		BaseStation baseStations[] = new BaseStation[50]; 
@@ -158,7 +166,6 @@ class Simulation{
 			baseStations[i].setyCoordinate(yCoordinate);	
 		}
 
-
 		// Creating 100 User Equipments with ids ranging from 1 to 100
 		UserEquipment userEquipments[] = new UserEquipment[100];
 		for(int i =0; i<100; i++){
@@ -184,30 +191,69 @@ class Simulation{
 			userEquipments[i].setxCoordinate(xCoordinate);
 			userEquipments[i].setyCoordinate(yCoordinate);
 		}
+		
+		// Assigning Base Stations to UEs based on least distance metric
+		for(int i=0; i<100; i++){
+			double minDistance = 1000000;
+			UserEquipment u = userEquipments[i];
+			int xUE = u.getxCoordinate();
+			int yUE = u.getyCoordinate();
+			int minBSId = 0;
 
-		// Printing out details of created BaseStations
-		System.out.println("Created BaseStation Details:");
-		for(int i=0; i<50 ; i++){
-			BaseStation b = baseStations[i]; 
-			System.out.println("BS Id: " + b.getId() + " at (" + b.getxCoordinate() + "," +
-			  + b.getyCoordinate() + ")");
+			for(int j=0; j<50; j++){
+				BaseStation b = baseStations[j];
+				int xBS = b.getxCoordinate();
+				int yBS = b.getyCoordinate();
+				double distance = Math.sqrt((xUE-xBS)*(xUE-xBS) + (yUE-yBS)*(yUE-yBS));
+				if(distance<=minDistance){
+					minDistance = distance;
+					minBSId = b.getId();
+				}
+			}
+
+			u.setassignedBSId(minBSId);
+			u.setassignedBSDist(minDistance);
+
+			baseStations[minBSId-1].addUserEquipment(u.getId());
 		}
 
-		// Printing out details of created UserEquipments
-		System.out.println();
-		System.out.println();
 
-		System.out.println("Created UserEquipment Details:");
-		for(int i=0; i<100 ; i++){
-			UserEquipment u = userEquipments[i]; 
-			System.out.println("UE Id: " + u.getId() + " at (" + u.getxCoordinate() + "," +
-			  + u.getyCoordinate() + ")");
+		// Writing output to file
+		try{
+			PrintStream fileOut = new PrintStream("./result.txt");
+			System.setOut(fileOut);
+
+			System.out.println("Ruuning Cellular Wireless Network Simulation");
+			System.out.println();
+			System.out.println();
+
+			System.out.println("                            User Equipment Details         ");
 			
-		}
+			System.out.println("UE Id     " + "x-Coordinate " + " y-Coordinate  " + "Connected to BSId      " 
+				+ "Distance from nearest BaseStation");
+			for(int i=0; i<100; i++){
+				UserEquipment u = userEquipments[i];
+				System.out.println(u.getId() + "                " + u.getxCoordinate() + "         " + 
+					u.getyCoordinate() + "               " + u.getassignedBSId() + "                    " 
+					+ u.getassignedBSDist());
+			}
 
+			System.out.println();
+			System.out.println();
 
+			System.out.println("                       Base Station Details         ");
+			
+			System.out.println("BSId          " +  "x-Coordinate " + " y-Coordinate  " + "List of Connected UEs based on nearedt distance metric  ");
+			for(int i =0; i<50; i++){
+				BaseStation b = baseStations[i];
+				ArrayList<Integer> ids = b.getConnectedUEIds();
+				System.out.println(b.getId() + "              " + "    "+b.getxCoordinate() + 
+					"          " + b.getyCoordinate() + "               "+ ids);
+			}
+		}catch(FileNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
 	}
-
-
 
 }
