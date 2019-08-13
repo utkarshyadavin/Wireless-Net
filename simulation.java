ac8aae25f9acd73882e4bed2483555eb6478be2f
utkarshyadavin <utkarshyadavin@gmail.com>
2019-08-13 22:29:49 +0530

    Created BS and UE objects and assigned positions

--- simulation.java
+++ simulation.java
@@ -5,13 +5,13 @@ class BaseStation{
 	int xCoordinate; 
 	int yCoordinate;
 	int id; 
-	ArrayList<Integer> connectedUserEquipmentIds; 
+	ArrayList<Integer> connectedUEIds; 
 
 	public BaseStation(){
 		this.xCoordinate = 0; 
 		this.yCoordinate = 0; 
 		this.id = -1;
-		this.connectedUserEquipmentIds = new ArrayList<Integer>;
+		this.connectedUEIds = new ArrayList<Integer>();
 	}
 
 	// Get BaseStation Id
@@ -46,12 +46,12 @@ class BaseStation{
 
 	// Add UserEuipment to the list of connected devices
 	public void addUserEquipment(int id){
-		connectedUserEquipmentIds.add(id);
+		connectedUEIds.add(id);
 	}
 
 	// Get the list of connected User Equipments;
-	public ArrayList<Integer> getConnectedUserEquipmentIds(){
-		return this.connectedUserEquipmentIds; 
+	public ArrayList<Integer> getConnectedUEIds(){
+		return this.connectedUEIds; 
 	}
 
 }
@@ -63,17 +63,17 @@ class UserEquipment{
 	int xCoordinate; 
 	int yCoordinate;
 	int id; 
-	int assignedBaseStationId;
+	int assignedBSId;
 
 	public UserEquipment(){
 		this.xCoordinate = 0; 
 		this.yCoordinate = 0; 
 		this.id = -1; 
-		this.assignedBaseStationId = -1;
+		this.assignedBSId = -1;
 	}
 
 	// Get UserEquipment Id
-	public int geId(){
+	public int getId(){
 		return this.id;
 	}
 
@@ -103,18 +103,111 @@ class UserEquipment{
 	}
 
 	// Get the id of the assigned base station the the User Equipment
-	public int getassignedBaseStationId(){
-		return this.assignedBaseStationId;
+	public int getassignedBSId(){
+		return this.assignedBSId;
 	}
 
 	// Assigne a particular base station id to a User Equipment
-	public void setassignedBaseStationId(int value){
-		this.assignedBaseStationId = value;
+	public void setassignedBSId(int value){
+		this.assignedBSId = value;
 	}
 
 }
 
 
 class Simulation{
+
+	// Generates a random number between specified max and min values
+	public static int generateRandom(int max, int min){
+		int temp = (int) (Math.random()*(max - min + 1) + min);
+		return temp;
+	}
+
+
+	public static void main(String args[]){
+
+		System.out.println("Ruuning Cellular Wireless Network Simulation");
+		// Maps to keep track of coordinates of BSTs and UEs to avoid repetition
+		HashMap<Integer, Integer> baseStationPosition = new HashMap<>();
+		HashMap<Integer,Integer> userEquipmentPosition = new HashMap<>();
+
+
+		// Create 50 Base Stations with ids ranging from 1 to 50
+		BaseStation baseStations[] = new BaseStation[50]; 
+		for(int i =0; i<50; i++){
+			baseStations[i] = new BaseStation(); 
+			baseStations[i].setId(i+1);
+
+			// Randomly generating x and y coordinates
+			int xCoordinate = generateRandom(100, -100);
+			int yCoordinate = generateRandom(100, -100);
+			
+			// Check for unique x and y co-ordinates
+			while(baseStationPosition.containsKey(xCoordinate)){
+				Integer temp = baseStationPosition.get(xCoordinate);
+				if(temp == yCoordinate){
+					xCoordinate = generateRandom(100, -100);
+					yCoordinate = generateRandom(100, -100);
+				}else{
+					break;
+				}
+			}
+
+			baseStationPosition.put(xCoordinate, yCoordinate);
+			baseStations[i].setxCoordinate(xCoordinate);
+			baseStations[i].setyCoordinate(yCoordinate);	
+		}
+
+
+		// Creating 100 User Equipments with ids ranging from 1 to 100
+		UserEquipment userEquipments[] = new UserEquipment[100];
+		for(int i =0; i<100; i++){
+			userEquipments[i] = new UserEquipment();
+			userEquipments[i].setId(i+1);
+
+			// Randomly generating x and y coordinates
+			int xCoordinate = generateRandom(100, -100);
+			int yCoordinate = generateRandom(100, -100);
+			
+			// Check for unique x and y co-ordinates
+			while(userEquipmentPosition.containsKey(xCoordinate)){
+				Integer temp = userEquipmentPosition.get(xCoordinate);
+				if(temp == yCoordinate){
+					xCoordinate = generateRandom(100, -100);
+					yCoordinate = generateRandom(100, -100);
+				}else{
+					break;
+				}
+			}
+
+			userEquipmentPosition.put(xCoordinate, yCoordinate);
+			userEquipments[i].setxCoordinate(xCoordinate);
+			userEquipments[i].setyCoordinate(yCoordinate);
+		}
+
+		// Printing out details of created BaseStations
+		System.out.println("Created BaseStation Details:");
+		for(int i=0; i<50 ; i++){
+			BaseStation b = baseStations[i]; 
+			System.out.println("BS Id: " + b.getId() + " at (" + b.getxCoordinate() + "," +
+			  + b.getyCoordinate() + ")");
+		}
+
+		// Printing out details of created UserEquipments
+		System.out.println();
+		System.out.println();
+
+		System.out.println("Created UserEquipment Details:");
+		for(int i=0; i<100 ; i++){
+			UserEquipment u = userEquipments[i]; 
+			System.out.println("UE Id: " + u.getId() + " at (" + u.getxCoordinate() + "," +
+			  + u.getyCoordinate() + ")");
+			
+		}
+
+
+	}
+
+
 
 }
